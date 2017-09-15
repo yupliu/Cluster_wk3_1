485b60eaaefadafdd257da8d0500f00b5cc9a10d
Alan Yu <yupliu@users.noreply.github.com>
2017-09-15 11:25:37 -0400

    Progress

--- Cluster_wk3_1/Cluster_wk3_1/Cluster_wk3_1.py
+++ Cluster_wk3_1/Cluster_wk3_1/Cluster_wk3_1.py
@@ -95,6 +95,8 @@ else:
     print('Check your code again')
 
 # Students should write code here
+distances = pairwise_distances(tf_idf,tf_idf[0:2,:],metric='euclidean')
+closest_cluster = np.argmin(distances,axis=1)
 
 '''Test cell'''
 reference = [list(row).index(min(row)) for row in distances]
@@ -102,3 +104,158 @@ if np.allclose(closest_cluster, reference):
     print('Pass')
 else:
     print('Check your code again')
+
+# Students should write code here
+queries = tf_idf[0:3,:]
+distances = pairwise_distances(tf_idf,queries,metric='euclidean')
+cluster_assignment = np.argmin(distances,axis=1)
+
+if len(cluster_assignment)==59071 and \
+   np.array_equal(np.bincount(cluster_assignment), np.array([23061, 10086, 25924])):
+    print('Pass') # count number of data points for each cluster
+else:
+    print('Check your code again.')
+
+def assign_clusters(data, centroids):
+    
+    # Compute distances between each data point and the set of centroids:
+    # Fill in the blank (RHS only)
+    distances_from_centroids = pairwise_distances(data,centroids,metric='euclidean')
+    
+    # Compute cluster assignments for each data point:
+    # Fill in the blank (RHS only)
+    cluster_assignment = np.argmin(distances_from_centroids,axis=1)
+    
+    return cluster_assignment
+
+if np.allclose(assign_clusters(tf_idf[0:100:10], tf_idf[0:8:2]), np.array([0, 1, 1, 0, 0, 2, 0, 2, 2, 1])):
+    print('Pass')
+else:
+    print('Check your code again.')
+
+data = np.array([[1., 2., 0.],
+                 [0., 0., 0.],
+                 [2., 2., 0.]])
+centroids = np.array([[0.5, 0.5, 0.],
+                      [0., -0.5, 0.]])
+
+cluster_assignment = assign_clusters(data, centroids)
+print cluster_assignment
+
+def revise_centroids(data, k, cluster_assignment):
+    new_centroids = []
+    for i in xrange(k):
+        # Select all data points that belong to cluster i. Fill in the blank (RHS only)
+        member_data_points = data[cluster_assignment==i]
+        # Compute the mean of the data points. Fill in the blank (RHS only)
+        centroid = member_data_points.mean(axis=0)
+        
+        # Convert numpy.matrix type to numpy.ndarray type
+        centroid = centroid.A1
+        new_centroids.append(centroid)
+    new_centroids = np.array(new_centroids)
+    
+    return new_centroids
+
+result = revise_centroids(tf_idf[0:100:10], 3, np.array([0, 1, 1, 0, 0, 2, 0, 2, 2, 1]))
+if np.allclose(result[0], np.mean(tf_idf[[0,30,40,60]].toarray(), axis=0)) and \
+   np.allclose(result[1], np.mean(tf_idf[[10,20,90]].toarray(), axis=0))   and \
+   np.allclose(result[2], np.mean(tf_idf[[50,70,80]].toarray(), axis=0)):
+    print('Pass')
+else:
+    print('Check your code')
+
+def compute_heterogeneity(data, k, centroids, cluster_assignment):
+    
+    heterogeneity = 0.0
+    for i in xrange(k):
+        
+        # Select all data points that belong to cluster i. Fill in the blank (RHS only)
+        member_data_points = data[cluster_assignment==i, :]
+        
+        if member_data_points.shape[0] > 0: # check if i-th cluster is non-empty
+            # Compute distances from centroid to data points (RHS only)
+            distances = pairwise_distances(member_data_points, [centroids[i]], metric='euclidean')
+            squared_distances = distances**2
+            heterogeneity += np.sum(squared_distances)
+        
+    return heterogeneity
+
+compute_heterogeneity(data, 2, centroids, cluster_assignment)
+
+# Fill in the blanks
+def kmeans(data, k, initial_centroids, maxiter, record_heterogeneity=None, verbose=False):
+    '''This function runs k-means on given data and initial set of centroids.
+       maxiter: maximum number of iterations to run.
+       record_heterogeneity: (optional) a list, to store the history of heterogeneity as function of iterations
+                             if None, do not store the history.
+       verbose: if True, print how many data points changed their cluster labels in each iteration'''
+    centroids = initial_centroids[:]
+    prev_cluster_assignment = None
+    
+    for itr in xrange(maxiter):        
+        if verbose:
+            print(itr)
+        
+        # 1. Make cluster assignments using nearest centroids
+        # YOUR CODE HERE
+        cluster_assignment = assign_clusters(data,centroids)
+            
+        # 2. Compute a new centroid for each of the k clusters, averaging all data points assigned to that cluster.
+        # YOUR CODE HERE
+        centroids = revise_centroids(data,k,cluster_assignment)
+            
+        # Check for convergence: if none of the assignments changed, stop
+        if prev_cluster_assignment is not None and (prev_cluster_assignment==cluster_assignment).all():
+            break
+        
+        # Print number of new assignments 
+        if prev_cluster_assignment is not None:
+            num_changed = np.sum(prev_cluster_assignment!=cluster_assignment)
+            if verbose:
+                print('    {0:5d} elements changed their cluster assignment.'.format(num_changed))   
+        
+        # Record heterogeneity convergence metric
+        if record_heterogeneity is not None:
+            # YOUR CODE HERE
+            score = compute_heterogeneity(data,k,centroids,cluster_assignment)
+            record_heterogeneity.append(score)
+        
+        prev_cluster_assignment = cluster_assignment[:]
+        
+    return centroids, cluster_assignment
+
+def plot_heterogeneity(heterogeneity, k):
+    plt.figure(figsize=(7,4))
+    plt.plot(heterogeneity, linewidth=4)
+    plt.xlabel('# Iterations')
+    plt.ylabel('Heterogeneity')
+    plt.title('Heterogeneity of clustering over time, K={0:d}'.format(k))
+    plt.rcParams.update({'font.size': 16})
+    plt.tight_layout()
+    plt.show()
+    plt.close()
+
+k = 3
+heterogeneity = []
+initial_centroids = get_initial_centroids(tf_idf, k, seed=0)
+centroids, cluster_assignment = kmeans(tf_idf, k, initial_centroids, maxiter=400,
+                                       record_heterogeneity=heterogeneity, verbose=True)
+plot_heterogeneity(heterogeneity, k)
+np.bincount(cluster_assignment)
+
+k = 10
+heterogeneity = {}
+import time
+start = time.time()
+for seed in [0, 20000, 40000, 60000, 80000, 100000, 120000]:
+    initial_centroids = get_initial_centroids(tf_idf, k, seed)
+    centroids, cluster_assignment = kmeans(tf_idf, k, initial_centroids, maxiter=400,
+                                           record_heterogeneity=None, verbose=False)
+    # To save time, compute heterogeneity only once in the end
+    heterogeneity[seed] = compute_heterogeneity(tf_idf, k, centroids, cluster_assignment)
+    print('seed={0:06d}, heterogeneity={1:.5f}'.format(seed, heterogeneity[seed]))
+    print np.bincount(cluster_assignment)
+    sys.stdout.flush()
+end = time.time()
+print(end-start)
